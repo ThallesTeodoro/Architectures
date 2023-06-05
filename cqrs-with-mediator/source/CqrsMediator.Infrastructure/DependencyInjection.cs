37ab591f8ef4bb358bbfd5f603708352e1fd930e
Thalles Teodoro <thallesmjteodoro@outlook.com>
2023-06-05 17:36:47 -0300

    🐳 chore: setup dependency injection of clean architecture

---
+++ cqrs-with-mediator/source/CqrsMediator.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CqrsMediator.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    }
+}
--- cqrs-with-mediator/source/CqrsMediator.Presentation/Program.cs
+++ cqrs-with-mediator/source/CqrsMediator.Presentation/Program.cs
@@ -1,24 +1,30 @@
-var builder = WebApplication.CreateBuilder(args);
+using CqrsMediator.Application;
+using CqrsMediator.Infrastructure;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    .AddInfrastructure()
+    .AddApplication();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+string basePath = "/api";
+app.UsePathBase(basePath);
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint($"{basePath}/swagger/v1/swagger.json", "CqrsMediator.Presentation v1");
+        c.DefaultModelsExpandDepth(-1);
+    });
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
